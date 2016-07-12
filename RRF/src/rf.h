7bb6a72aa2046f5480999ce2e4a15099c5603d8c
guanxin1121 <xguan8@asu.edu>
2016-07-12 15:17:22 -0700

    Update rf.h

--- RRF/src/rf.h
+++ RRF/src/rf.h
@@ -48,7 +48,7 @@ void regTree(double *x, double *y, int mdim, int nsample,
 void findBestSplit(double *x, int *jdex, double *y, int mdim, int nsample, 
 		   int ndstart, int ndend, int *msplit, double *decsplit, 
 		   double *ubest, int *ndendl, int *jstat, int mtry,
-		   double sumnode, int nodecnt, int *cat);
+		   double sumnode, int nodecnt, int *cat, int *varUsedAll, double *coefReg, int *flagReg);
 
 void predictRegTree(double *x, int nsample, int mdim, 
 		    int *lDaughter, int *rDaughter, int *nodestatus, 
