e68c26e15397048eb3d66f92ef5797a42d0fb167
Houtao Deng <houtao@instacart.com>
2022-05-30 11:26:56 -0700

    update RRF_1.9.3 with cran version

--- RRF.Rcheck/00_pkg_src/RRF/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-    RFver <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
-                      fields="Version")
-    packageStartupMessage(paste(pkgname, RFver))
-    packageStartupMessage("Type rrfNews() to see new features/changes/bug fixes.")
-}
